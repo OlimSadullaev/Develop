7178735719056e9db4b6a72ceaf616b3ae1594a4
OlimSadullaev <91972198+OlimSadullaev@users.noreply.github.com>
2022-08-10 19:12:00 +0500

    Add files via upload

---
+++ RestaurantWebsite/Controllers/RestaurantWebsiteController.cs
@@ -0,0 +1,51 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantWebsite.Constollers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantWebsiteController : ControllerBase
+    {
+        public static IWebHostEnvironment _environment; 
+        public RestaurantWebsiteController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public class FileUPloadAPI
+        {
+            public IFormFile files { get; set; }
+        }
+
+        public async Task<string> Post(FileUPloadAPI objFile)
+        {
+            await Task.Delay(2000);
+            try
+            {
+                if (objFile.files.Length > 0)
+                {
+                    if (!Directory.Exists(_environment.WebRootPath + "\\Upload\\"))
+                    {
+                        Directory.CreateDirectory(_environment.WebRootPath + "\\Upload\\");
+                    }
+
+                    using (FileStream fileStream = System.IO.File.Create(_environment.WebRootPath + "\\Upload\\" + objFile.files.FileName))
+                    {
+                        objFile.files.CopyTo(fileStream);
+                        fileStream.Flush();
+                        return "\\Upload\\" + objFile.files.FileName;
+                    }
+                }
+                else
+                {
+                    return "Failed";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message.ToString();
+            }
+        }
+    }
+}
